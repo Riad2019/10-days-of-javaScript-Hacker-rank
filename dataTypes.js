5236a544a98788152134f015722fc51b6378a719
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-12-23 18:55:48 +0800

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,16 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+
+    console.log(firstInteger + parseInt(secondInteger))
+
+    console.log(firstDecimal + parseFloat(secondDecimal))
+
+    console.log(firstString + secondString)
+}
